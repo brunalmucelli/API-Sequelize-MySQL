167a603cb2fec8bab1eebd7e508258248e13aace
brunalmucelli <brunalmucelli@gmail.com>
2023-05-19 19:08:39 -0300

    migração adição de colunas

--- api/models/niveis.js
+++ api/models/niveis.js
@@ -5,7 +5,7 @@ const pessoas = require("./pessoas");
 module.exports = (sequelize, DataTypes) => {
   const Niveis = sequelize.define('Niveis', {
     descr_nivel: DataTypes.STRING
-  }, {});
+  }, {paranoid : true});
   Niveis.associate = function(models) {
     Niveis.hasMany(models.Turmas, {
       foreignKey: "nivel_id"
