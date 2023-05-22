b95662537f08605f0b5f4e90ea85ff238a520b59
brunalmucelli <brunalmucelli@gmail.com>
2023-05-22 18:23:03 -0300

    definição de outro escopo

--- api/models/pessoas.js
+++ api/models/pessoas.js
@@ -8,9 +8,10 @@ module.exports = (sequelize, DataTypes) => {
   }, {
     paranoid: true,
     defaultScope: {
-      where: {
-        ativo: true
-      }
+      where: { ativo: true }
+    },
+    scopes: {
+      todos: { where: {} }
     }
   })
   Pessoas.associate = function (models) {
--- api/routes/pessoasRoute.js
+++ api/routes/pessoasRoute.js
@@ -5,7 +5,8 @@ const pessoas = require('../models/pessoas')
 const router = Router()
 
 router
-  .get('/pessoas', PessoaController.pegaTodasAsPessoas)
+  .get('/pessoas', PessoaController.pegaPessoasAtivas)
+  .get('/pessoas/todos',PessoaController.pegaTodasAsPessoas)
   .get('/pessoas/:id', PessoaController.pegaUmaPessoa)
   .post('/pessoas', PessoaController.criaPessoa)
   .put('/pessoas/:id', PessoaController.atualizaPessoa)
